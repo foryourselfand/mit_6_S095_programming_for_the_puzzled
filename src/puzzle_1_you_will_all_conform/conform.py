fae1b4b74d6226f858d744fced01f23b2ec425c3
foryourselfand <foryourselfand@gmail.com>
2020-01-16 22:54:22 +0300

    main my (bad) solution

--- src/puzzle_1_you_will_all_conform/conform.py
+++ src/puzzle_1_you_will_all_conform/conform.py
@@ -4,9 +4,34 @@
 # Output is a set of commands (printed out) to get either all F's or all B's
 # Fewest commands are the goal
 
-caps = ['F', 'F', 'B', 'B', 'B', 'F', 'B',
-        'B', 'B', 'F', 'F', 'B', 'F']
-cap2 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'F', 'F']
+from collections import defaultdict
+
+
+def pleaseConformMy(caps):
+    caps.append('end')
+    
+    interval_count = defaultdict(int)
+    cap_last = caps[0]
+    
+    for cap_current in caps[1:]:
+        if cap_current != cap_last:
+            interval_count[cap_last] += 1
+            cap_last = cap_current
+    
+    interval_min = min(interval_count.items(), key = lambda item: item[1])
+    cap_min = interval_min[0]
+    
+    index_start = 0
+    cap_previous = caps[0]
+    for index_current, cap_current in enumerate(caps[1:], 1):
+        if cap_current != cap_min:
+            if cap_previous == cap_min:
+                index_end = index_current - 1
+                print(f'People in positions {index_start} through {index_end} flip your caps!')
+            index_start = index_current + 1
+        
+        cap_previous = cap_current
+    print()
 
 
 def pleaseConform(caps):
@@ -15,7 +40,7 @@ def pleaseConform(caps):
     forward = 0
     backward = 0
     intervals = []
-
+    
     # Determine intervals where caps are on in the same direction
     for i in range(1, len(caps)):
         if caps[start] != caps[i]:
@@ -27,7 +52,7 @@ def pleaseConform(caps):
             else:
                 backward += 1
             start = i
-
+    
     # Need to add the last interval after for loop completes execution
     intervals.append((start, len(caps) - 1, caps[start]))
     if caps[start] == 'F':
@@ -129,5 +154,14 @@ def pleaseConformExercise(caps):
             else:
                 print('People in positions', t[0], 'through', t[1], 'flip your caps!')
 
-pleaseConform(caps)
-##pleaseConform(cap2)
+
+def main() -> None:
+    caps1 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'B', 'F']
+    caps2 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'F', 'F']
+    
+    pleaseConform(caps1)
+    pleaseConform(caps2)
+
+
+if __name__ == '__main__':
+    main()
