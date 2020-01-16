54b9bba04d3674c9585388f9682a6eeaafce4026
foryourselfand <foryourselfand@gmail.com>
2020-01-16 22:52:31 +0300

    conform_opt and line_exercise1 -> conform

--- src/puzzle_1_you_will_all_conform/conform.py
+++ src/puzzle_1_you_will_all_conform/conform.py
@@ -48,5 +48,86 @@ def pleaseConform(caps):
                   'through', t[1], 'flip your caps!')
 
 
+def pleaseConformOpt(caps):
+    # Initialization
+    start = 0
+    forward = 0
+    backward = 0
+    intervals = []
+    
+    caps = caps + ['END']
+    
+    # Determine intervals where caps are on in the same direction
+    for i in range(1, len(caps)):
+        if caps[start] != caps[i]:
+            # each interval is a tuple with 3 elements (start, end, type)
+            intervals.append((start, i - 1, caps[start]))
+            
+            if caps[start] == 'F':
+                forward += 1
+            else:
+                backward += 1
+            start = i
+    
+    if forward < backward:
+        flip = 'F'
+    else:
+        flip = 'B'
+    for t in intervals:
+        if t[2] == flip:
+            # Exercise: if t[0] == t[1] change the printing!
+            print('People in positions', t[0], 'through', t[1], 'flip your caps!')
+
+
+def pleaseConformOnepass(caps):
+    caps = caps + [caps[0]]
+    for i in range(1, len(caps)):
+        if caps[i] != caps[i - 1]:
+            if caps[i] != caps[0]:
+                print('People in positions', i, end = '')
+            else:
+                print(' through', i - 1, 'flip your caps!')
+
+
+def pleaseConformExercise(caps):
+    # Initialization
+    start = 0
+    forward = 0
+    backward = 0
+    intervals = []
+    
+    # Determine intervals where caps are on in the same direction
+    for i in range(len(caps)):
+        if caps[start] != caps[i]:
+            # each interval is a tuple with 3 elements (start, end, type)
+            intervals.append((start, i - 1, caps[start]))
+            
+            if caps[start] == 'F':
+                forward += 1
+            else:
+                backward += 1
+            start = i
+    
+    # Need to add the last interval after for loop completes execution
+    intervals.append((start, len(caps) - 1, caps[start]))
+    if caps[start] == 'F':
+        forward += 1
+    else:
+        backward += 1
+    
+    ##    print (intervals)
+    ##    print (forward, backward)
+    if forward < backward:
+        flip = 'F'
+    else:
+        flip = 'B'
+    for t in intervals:
+        if t[2] == flip:
+            # Exercise: if t[0] == t[1] change the printing!
+            if t[0] == t[1]:
+                print('Person at position', t[0], 'flip your cap!')
+            else:
+                print('People in positions', t[0], 'through', t[1], 'flip your caps!')
+
 pleaseConform(caps)
 ##pleaseConform(cap2)
